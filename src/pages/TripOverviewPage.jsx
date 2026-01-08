ada178c94af4a6ed5325cd7355806211a5b64acb
Nikoleta Papoutsi <nikoletapapoutsi19@gmail.com>
2026-01-08 12:13:55 +0200

    Update TripOverviewPage.jsx

--- src/pages/TripOverviewPage.jsx
+++ src/pages/TripOverviewPage.jsx
@@ -199,9 +199,9 @@ const TripOverviewPage = () => {
 				marginBottom: '0.75rem',
 				fontSize: '0.8rem',
 				color: 'var(--color-danger)',
-						 backgroundColor: 'var(--color-danger-soft)',
-						 padding: '0.4rem 0.6rem',
-						 borderRadius: '0.75rem'
+				backgroundColor: 'var(--color-danger-soft)',
+				padding: '0.4rem 0.6rem',
+				borderRadius: '0.75rem'
 			}}
 			>
 			{serverError}
